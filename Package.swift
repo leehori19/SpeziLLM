156dfd74d9567c4da095a8272aa412de6833bfff
leeho <102628788+leehori19@users.noreply.github.com>
2025-07-26 14:07:21 -1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,12 +22,10 @@ let package = Package(
         .library(name: "SpeziLLM", targets: ["SpeziLLM"]),
         .library(name: "SpeziLLMLocal", targets: ["SpeziLLMLocal"]),
         .library(name: "SpeziLLMLocalDownload", targets: ["SpeziLLMLocalDownload"]),
-        .library(name: "SpeziLLMOpenAI", targets: ["SpeziLLMOpenAI"]),
-        .library(name: "SpeziLLMFog", targets: ["SpeziLLMFog"])
     ],
     dependencies: [
         .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.21.2")),
-        .package(url: "https://github.com/ml-explore/mlx-swift-examples", exact: "2.21.2"),  // Pin MLX Swift Examples as it doesn't follow semantic versioning
+        .package(url: "https://github.com/ml-explore/mlx-swift-examples", exact: "2.25.5"),  // Pin MLX Swift Examples as it doesn't follow semantic versioning
         .package(url: "https://github.com/huggingface/swift-transformers", .upToNextMinor(from: "0.1.14")),
         .package(url: "https://github.com/StanfordSpezi/Spezi", from: "1.8.0"),
         .package(url: "https://github.com/StanfordSpezi/SpeziFoundation", from: "2.1.0"),
@@ -37,7 +35,6 @@ let package = Package(
         .package(url: "https://github.com/StanfordSpezi/SpeziViews", from: "1.8.0"),
         .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.0"),
-        .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.2")
     ],
     targets: [
         .target(
@@ -69,45 +66,6 @@ let package = Package(
                 .product(name: "SpeziViews", package: "SpeziViews"),
                 .target(name: "SpeziLLMLocal"),
                 .product(name: "MLXLLM", package: "mlx-swift-examples")
-            ],
-            swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
-        ),
-        .target(
-            name: "SpeziLLMOpenAI",
-            dependencies: [
-                .target(name: "SpeziLLM"),
-                .target(name: "GeneratedOpenAIClient"),
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
-                .product(name: "SpeziFoundation", package: "SpeziFoundation"),
-                .product(name: "Spezi", package: "Spezi"),
-                .product(name: "SpeziChat", package: "SpeziChat"),
-                .product(name: "SpeziKeychainStorage", package: "SpeziStorage"),
-                .product(name: "SpeziOnboarding", package: "SpeziOnboarding")
-            ],
-            swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
-        ),
-        .target(
-            name: "SpeziLLMFog",
-            dependencies: [
-                .target(name: "SpeziLLM"),
-                .target(name: "GeneratedOpenAIClient"),
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
-                .product(name: "Spezi", package: "Spezi")
-            ],
-            swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
-        ),
-        .target(
-            name: "GeneratedOpenAIClient",
-            plugins: [
-                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
-            ]
-        ),
-        .testTarget(
-            name: "SpeziLLMTests",
-            dependencies: [
-                .target(name: "SpeziLLMOpenAI")
             ],
             swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
         )
